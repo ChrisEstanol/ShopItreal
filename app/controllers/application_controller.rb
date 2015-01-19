f18b64cf76c40665d3cfdbb107350c42515aae9f
Christophe E <cestanol@gmail.com>
2015-01-19 16:57:32 -0500

    Define search variable globally throught application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :set_search
   protect_from_forgery with: :exception
 
   layout "admin", if: :devise_controller?, only: :edit
+
+  def set_search
+    @search = Product.search(params[:q])
+  end
 
 end
